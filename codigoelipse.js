d887fe14601a3370d67009bf2b39ea01268bfe31
ricardo9516 <42320206+ricardo9516@users.noreply.github.com>
2019-01-23 20:33:34 -0600

    Create codigoelipse.js
    Se muestra circulo rojo

---
+++ codigoelipse.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+    background(255,255,255);
+    stroke(125,0,0);
+    strokeWeight(10);
+  	ellipse(100,100,100,100);
+  	ellipse(300,100,100,100);
+	  fill(0,0,0);
+  	ellipse(300,300,100,100);
+    ellipse(100,300,100,100);
+}
