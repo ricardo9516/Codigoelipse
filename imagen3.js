1769d6a6184d7f02988a719fd8689a2b34ddd626
ricardo9516 <42320206+ricardo9516@users.noreply.github.com>
2019-01-28 17:05:06 -0600

    Create imagen3.js
    imagen3

---
+++ imagen3.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	fill(255);
+	stroke(255,0,0);
+	strokeWeight(20);
+	ellipse(200,200,250,250);
+	line(120,120,280,280);
+}
