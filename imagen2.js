29fc7fe9f9819e4e56c2441b58f48a600e360a9b
ricardo9516 <42320206+ricardo9516@users.noreply.github.com>
2019-01-28 17:01:11 -0600

    Create imagen2.js
    Imagen 2

---
+++ imagen2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(255);
+	fill(255,0,0);
+	rect(0,0,100,300);
+	fill(0,255,0);
+	rect(100,0,200,300);
+	fill(0,0,255);
+	rect(200,0,100,300);
+}
